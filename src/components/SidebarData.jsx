cbbccb3c5f386c57de9afe3b0725f7785a1f73b2
Kavyashah119 <kavyashah2929@gmail.com>
2021-04-12 17:26:00 +0530

    Initial commit

---
+++ src/components/SidebarData.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+
+    {
+        title: 'Dashboard',
+        path: '/',
+        icon: <AiIcons.AiFillDashboard />,
+        cName: 'nav-text'
+    },
+
+    {
+        title: 'My Courses',
+        path: '/',
+        icon: <FaIcons.FaBookReader />,
+        cName: 'nav-text'
+    },
+
+    {
+        title: 'Achievements',
+        path: '/',
+        icon: <FaIcons.FaTrophy />,
+        cName: 'nav-text'
+    },
+
+    {
+        title: 'Forum',
+        path: '/',
+        icon: <FaIcons.FaCommentAlt />,
+        cName: 'nav-text'
+    }
+]
